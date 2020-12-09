97b1b32ce27393860b30c7bab008c73fbf0e2a15
root <bhautikrchudasama@gmail.com>
2020-12-09 06:50:18 +0100

    v1.0.2

---
+++ app/src/main/java/in/rmtsbus/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package in.rmtsbus.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
